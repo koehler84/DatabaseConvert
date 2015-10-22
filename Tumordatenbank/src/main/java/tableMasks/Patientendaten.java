9c83c7857b3ecbd40e64618c3e55bc0b6f16f759
Eike Wichern <e.wichern@outlook.de>
2015-10-22 10:18:46 +0200

    added class patientendaten for table in FX

---
+++ Tumordatenbank/src/main/java/tableMasks/Patientendaten.java
@@ -0,0 +1,18 @@
+package tableMasks;
+
+import java.sql.Date;
+
+public class Patientendaten {
+
+	Date geburtsdatum;
+	String vorname;
+	String name;
+	String strasse;
+	String hausnummer;
+	String land;
+	String plz;
+	String Ort;
+	
+	
+	
+}
