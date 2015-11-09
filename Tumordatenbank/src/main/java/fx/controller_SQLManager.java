13160401c31da0648d32878001c9aac9c6e8cd1b
eike <eike@test.net>
2015-11-09 17:30:27 +0100

    added table functionality for panelSQLManager

--- Tumordatenbank/src/main/java/fx/controller_SQLManager.java
+++ Tumordatenbank/src/main/java/fx/controller_SQLManager.java
@@ -2,100 +2,64 @@ package fx;
 
 import java.net.URL;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.util.Callback;
+import tableMasks.Prototyp_universell;
 
 public class controller_SQLManager implements Initializable {
 
 	@FXML public static AnchorPane mainPanel;
 	@FXML public TextField txtField_Statement;
-	@SuppressWarnings("rawtypes")
-	@FXML public ComboBox cmbBox_Statement;
-	@SuppressWarnings("rawtypes")
-	@FXML public TableView table;
-	@SuppressWarnings("rawtypes")
-	private ObservableList<ObservableList> data;
+	@FXML public ComboBox<String> cmbBox_Statement;
+	@FXML public TableView<Prototyp_universell> table;
+	//private ObservableList<Prototyp_universell> data;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 		System.out.println("controller SQLManager");
+		
+		table.getColumns().setAll(Prototyp_universell.getColumns());
 	}
 	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public void executeStatement() {
+
+//		System.out.println(cmbBox_Statement.getSelectionModel().getSelectedItem());
 		Statement st = null;
-		ResultSet res = null;
-		ResultSetMetaData resMeta = null;
-		data = FXCollections.observableArrayList();
+		ResultSet rs = null;
 		
-		/**********************************
-		* TABLE COLUMN ADDED DYNAMICALLY *
-		**********************************/
 		try {
 			st = FX_Main.cn.createStatement();
-			res = st.executeQuery("select * from fall;");
-			resMeta = res.getMetaData();
+			//rs = st.executeQuery(cmbBox_Statement.getSelectionModel().getSelectedItem());
+			rs = st.executeQuery("select patdaten.`Name`, patdaten.Vorname, patdaten.Geburtsdatum ,mydb.fall.`E.-Nummer`, mydb.klassifikation.ER, mydb.klassifikation.PR,  mydb.klassifikation.`Her2/neu`, mydb.klassifikation.lage, mydb.klassifikation.Tumorart,\r\n" + 
+					" klassifikation.G, klassifikation.T, klassifikation.N, klassifikation.N, klassifikation.M, klassifikation.L, klassifikation.V, klassifikation.R\r\n" + 
+					"from mydb.klassifikation as klassifikation\r\n" + 
+					"join mydb.fall as fall \r\n" + 
+					"join mydb.patientendaten as patdaten on klassifikation.`Fall_E.-Nummer` = fall.`E.-Nummer` and klassifikation.Fall_Befundtyp = fall.Befundtyp and patdaten.PatientenID = fall.Patientendaten_PatientenID;");
+			
+			while (rs.next()) {
+				LocalDate geburtsdatum = null;
+				if (rs.getDate("Geburtsdatum") != null) geburtsdatum = rs.getDate("Geburtsdatum").toLocalDate();
+				
+				Prototyp_universell data = new Prototyp_universell(geburtsdatum, rs.getString("Vorname"), rs.getString("Name"),
+						rs.getString("E.-Nummer"), rs.getString("ER"), rs.getString("PR"), rs.getString("Her2/neu"), 
+						rs.getString("Tumorart"), rs.getString("T"), rs.getString("N"), rs.getString("M"), 
+						rs.getInt("G"), rs.getInt("L"), rs.getInt("R"), rs.getInt("V"));
+				
+				table.getItems().add(data);
+			}
 		} catch (SQLException e) {
 			System.err.println(e + "\nFehler beim Statement/ResultSet in executeStatement() in " + getClass().getName());
 			return;
 		}
-		
-		table.getColumns().clear();
-		
-		try {
-			
-			for (int i = 0; i < resMeta.getColumnCount(); i++) {
-				
-				final int j = i;
-				TableColumn column = new TableColumn(resMeta.getColumnName(i+1));
-				column.setCellValueFactory(new Callback<CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
-					@Override
-					public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {
-						return new SimpleStringProperty(param.getValue().get(j).toString());
-					}
-				});
-				
-				table.getColumns().addAll(column);
-			}
-			
-			/********************************
-			* Data added to ObservableList *
-			********************************/
-			while (res.next()) {
-				
-				ObservableList<String> rowData = FXCollections.observableArrayList();
-				for (int i = 1; i < resMeta.getColumnCount(); i++) {
-					rowData.add(res.getString(i));
-				}				
-				data.add(rowData);
-				
-			}
-			
-			//table.setItems(data);
-			table.getItems().add(data);
-		} catch (Exception e) {
-			System.err.println("Fehler beim Erstellen der dynamischen Tabelle");
-			System.out.println(e);
-			return;
-		}
-		
 	}
 
 }
--- Tumordatenbank/src/main/java/tableMasks/Prototyp_universell.java
+++ Tumordatenbank/src/main/java/tableMasks/Prototyp_universell.java
@@ -1,6 +1,10 @@
 package tableMasks;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class Prototyp_universell {
 
@@ -12,13 +16,13 @@ public class Prototyp_universell {
 	String pr;
 	String her2_neu;
 	String tumorart;
-	String T;
-	String N;
-	String M;
-	int G;
-	int L;
-	int R;
-	int V;
+	String t;
+	String n;
+	String m;
+	int g;
+	int l;
+	int r;
+	int v;
 	
 	public Prototyp_universell(LocalDate geburtsdatum, String vorname, String name, String eNummer, String er,
 			String pr, String her2_neu, String tumorart, String t, String n, String m, int g, int l, int r, int v) {
@@ -26,19 +30,79 @@ public class Prototyp_universell {
 		this.geburtsdatum = geburtsdatum;
 		this.vorname = vorname;
 		this.name = name;
-		ENummer = eNummer;
+		this.ENummer = eNummer;
 		this.er = er;
 		this.pr = pr;
 		this.her2_neu = her2_neu;
 		this.tumorart = tumorart;
-		T = t;
-		N = n;
-		M = m;
-		G = g;
-		L = l;
-		R = r;
-		V = v;
+		this.t = t;
+		this.n = n;
+		this.m = m;
+		this.g = g;
+		this.l = l;
+		this.r = r;
+		this.v = v;
 	}
+	
+	/**
+	 * Use this method to get columns to add or set to a <i>TableView</i>.
+	 * @return ArrayList including <i>TableColumns</i>. To set the Columns in the <i>TableView</i> use: 
+	 * <i>table.getColumns().addAll(Prototyp_universell.getColumns());</i>
+	 */
+	public static ArrayList<TableColumn<Prototyp_universell, ?>> getColumns() {
+		
+		ArrayList<TableColumn<Prototyp_universell, ?>> columns = new ArrayList<TableColumn<Prototyp_universell, ?>>();
+		
+		TableColumn<Prototyp_universell, LocalDate> col_gebDatum = new TableColumn<Prototyp_universell, LocalDate>("Geburtsdatum");
+		col_gebDatum.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, LocalDate>("geburtsdatum"));
+		TableColumn<Prototyp_universell, String> col_vorname = new TableColumn<Prototyp_universell, String>("Vorname");
+		col_vorname.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, String>("vorname"));
+		TableColumn<Prototyp_universell, String> col_name = new TableColumn<Prototyp_universell, String>("Nachname");
+		col_name.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, String>("name"));
+		TableColumn<Prototyp_universell, String> col_ENummer = new TableColumn<Prototyp_universell, String>("E.-Nummer");
+		col_ENummer.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, String>("ENummer"));
+		TableColumn<Prototyp_universell, String> col_er = new TableColumn<Prototyp_universell, String>("Östrogen Rezeptor");
+		col_er.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, String>("er"));
+		TableColumn<Prototyp_universell, String> col_pr = new TableColumn<Prototyp_universell, String>("Progesteron Rezeptor");
+		col_pr.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, String>("pr"));
+		TableColumn<Prototyp_universell, String> col_her2_neu = new TableColumn<Prototyp_universell, String>("Her2/neu");
+		col_her2_neu.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, String>("her2_neu"));
+		TableColumn<Prototyp_universell, String> col_tumorart = new TableColumn<Prototyp_universell, String>("Tumorart");
+		col_tumorart.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, String>("tumorart"));
+		TableColumn<Prototyp_universell, String> col_T = new TableColumn<Prototyp_universell, String>("T");
+		col_T.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, String>("t"));
+		TableColumn<Prototyp_universell, String> col_N = new TableColumn<Prototyp_universell, String>("N");
+		col_N.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, String>("n"));
+		TableColumn<Prototyp_universell, String> col_M = new TableColumn<Prototyp_universell, String>("M");
+		col_M.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, String>("m"));
+		TableColumn<Prototyp_universell, String> col_G = new TableColumn<Prototyp_universell, String>("G");
+		col_G.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, String>("g"));
+		TableColumn<Prototyp_universell, String> col_L = new TableColumn<Prototyp_universell, String>("L");
+		col_L.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, String>("l"));
+		TableColumn<Prototyp_universell, String> col_R = new TableColumn<Prototyp_universell, String>("R");
+		col_R.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, String>("r"));
+		TableColumn<Prototyp_universell, String> col_V = new TableColumn<Prototyp_universell, String>("V");
+		col_V.setCellValueFactory(new PropertyValueFactory<Prototyp_universell, String>("v"));
+		
+		columns.add(col_gebDatum);
+		columns.add(col_vorname);
+		columns.add(col_name);
+		columns.add(col_ENummer);
+		columns.add(col_er);
+		columns.add(col_pr);
+		columns.add(col_her2_neu);
+		columns.add(col_tumorart);
+		columns.add(col_T);
+		columns.add(col_N);
+		columns.add(col_M);
+		columns.add(col_G);
+		columns.add(col_L);
+		columns.add(col_R);
+		columns.add(col_V);
+		
+		return columns;
+	}
+	
 	public LocalDate getGeburtsdatum() {
 		return geburtsdatum;
 	}
@@ -88,46 +152,46 @@ public class Prototyp_universell {
 		this.tumorart = tumorart;
 	}
 	public String getT() {
-		return T;
+		return t;
 	}
 	public void setT(String t) {
-		T = t;
+		this.t = t;
 	}
 	public String getN() {
-		return N;
+		return n;
 	}
 	public void setN(String n) {
-		N = n;
+		this.n = n;
 	}
 	public String getM() {
-		return M;
+		return m;
 	}
 	public void setM(String m) {
-		M = m;
+		this.m = m;
 	}
 	public int getG() {
-		return G;
+		return g;
 	}
 	public void setG(int g) {
-		G = g;
+		this.g = g;
 	}
 	public int getL() {
-		return L;
+		return l;
 	}
 	public void setL(int l) {
-		L = l;
+		this.l = l;
 	}
 	public int getR() {
-		return R;
+		return r;
 	}
 	public void setR(int r) {
-		R = r;
+		this.r = r;
 	}
 	public int getV() {
-		return V;
+		return v;
 	}
 	public void setV(int v) {
-		V = v;
+		this.v = v;
 	}
 	
 }
